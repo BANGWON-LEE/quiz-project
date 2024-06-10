7ed6418738a3affbd7c7791e397fdeeca98a1bce
BANGWON-LEE <qkddnjs31@gmail.com>
2024-06-11 00:03:36 +0900

    edit : 스타일 변경

--- src/components/common/text/TextFieldTitle.tsx
+++ src/components/common/text/TextFieldTitle.tsx
@@ -1,11 +1,11 @@
-"use client";
-import { TextFiledType } from "@/types/common";
-import React from "react";
+'use client'
+import { TextFiledType } from '@/types/common'
+import React from 'react'
 
 export default function TextFieldTitle(props: TextFiledType) {
-  const { text } = props;
+  const { text } = props
 
-  return <h2 className="text-[1.50rem] font-bold text-primary-200">{text}</h2>;
+  return <h2 className="text-[1.50rem] font-bold text-primary-400">{text}</h2>
 }
 
 // export default TextFieldTitle;
